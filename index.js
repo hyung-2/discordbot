e1bfff3e21cd59cf8a227f840e28e765f4587d87
hyung-2 <nh_0221@naver.com>
2024-04-18 17:26:51 +0900

    명령어 입력시 버튼으로 응답받고 개인dm보내기까지 완성

--- index.js
+++ index.js
@@ -1,41 +1,80 @@
-const fs = require('node:fs')
-const path = require('node:path')
-const { Client, Collection, GatewayIntentBits, } = require('discord.js')
 const {token} = require('./config.json')
 
-const client = new Client({intents: [GatewayIntentBits.Guilds]});
+const { Client, GatewayIntentBits, ActionRowBuilder, ButtonBuilder, ButtonStyle, } = require('discord.js')
+const client = new Client({intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.DirectMessages]});
 
-client.commands = new Collection()
+client.once('ready', () => {
+  console.log('봇 준비 완료!')
+})
 
-const foldersPath = path.join(__dirname, 'commands')
-const commandFolders = fs.readdirSync(foldersPath)
 
-for(const folder of commandFolders){
-  const commandsPath = path.join(foldersPath, folder)
-  const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'))
-  for(const file of commandFiles){
-    const filePath = path.join(commandsPath, file)
-    const command = require(filePath)
-    if('data' in command && 'execute' in command){
-      client.commands.set(command.data.name, command)
-    }else{
-      console.log(`[WARNING] The comamnd at ${filePath} is missing a required 'data' or 'execute' property.`)
+client.on('messageCreate', async message => {
+  await message.fetch()
+  console.log('메세지 수신됨', message.content)
+  // 멤버 목록 불러오기
+  if(message.content === '!멤버'){    
+    try{
+      const guild = message.guild
+      // console.log('길드', guild)
+      if(guild) {
+        // console.log('길드멤버',guild.members)
+        guild.members.cache.forEach(async member => {
+          console.log('멤버이름들',member.user.username)
+          if(member.user.username === '__moong'){
+            message.channel.send({
+              contents: member.user,
+              //버튼
+              components:[
+                new ActionRowBuilder({
+                  components: [
+                      
+                    new ButtonBuilder()
+                      .setCustomId('yes')
+                      .setLabel('가능')
+                      .setStyle(ButtonStyle.Primary),
+                    new ButtonBuilder()
+                      .setCustomId('no')
+                      .setLabel('불가능')
+                      .setStyle(ButtonStyle.Danger)
+                  ]
+                })
+              ]
+            }).then(sentMessage => {
+              //버튼 클릭 대기(24시간)
+              const collector = sentMessage.createMessageComponentCollector({ filter: i => i.user.id === user.id, time: 24 * 60 * 60 * 1000})
+  
+              collector.on('collect', async interaction => {
+                if(interaction.customId === 'yes'){
+                  await interaction.update({content: `${member.user} 가능`,  components:[]})
+                }else if(interaction.customId === 'no'){
+                  await interaction.update({content: `${member.user} 불가능`,  components:[]})
+                }
+              })
+              collector.on('end', () => {
+                if(!user.deleted) user.send('응답시간 종료')
+              })
+            })
+
+
+            const user = member.user
+            //개인 dm 보내기
+            try{
+              await user.send({
+                content: `멤버 스케쥴에 24시간 이내에 응답해주세요.`,
+              })
+            }catch(error){
+              console.error('개인 dm 오류', error)
+            }
+          }
+
+        })
+      }
+    }catch(error){
+      console.error('멤버목록 불러오는 중 오류', error)
     }
   }
-}
+})
 
-const eventsPath = path.join(__dirname, 'events');
-const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
-
-for (const file of eventFiles) {
-	const filePath = path.join(eventsPath, file);
-	const event = require(filePath);
-	if (event.once) {
-		client.once(event.name, (...args) => event.execute(...args));
-	} else {
-		client.on(event.name, (...args) => event.execute(...args));
-	}
-}
 
 
 client.login(token)
